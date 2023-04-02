51adc2aa29bffd4798022a0e58531daa09dbd30a
Web Dev Cody <webdevcody@gmail.com>
2023-04-02 09:59:56 -0400

    attempting to get the extension to parse the dom

--- popup.js
+++ popup.js
@@ -1,25 +1,38 @@
-console.log('Content script loaded');
+console.log("Content script loaded");
 
-document.addEventListener('DOMContentLoaded', function () {
-  const btnAction = document.getElementById('btnAction');
-  btnAction.addEventListener('click', function () {
+document.addEventListener("DOMContentLoaded", function () {
+  const btnAction = document.getElementById("btnAction");
+  btnAction.addEventListener("click", function () {
     const pageContent = parsePageContent();
-    console.log('Page content:', pageContent);
+    console.log("Page content:", pageContent);
   });
 });
 
-
 function parsePageContent() {
-  // You can use DOM manipulation to parse the content you want.
-  // For example, you can extract all the text inside paragraph tags:
-  const paragraphs = document.querySelectorAll('p');
-  let textContent = '';
+  // Get the current tab
+  chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+    // Get the current tab's URL and ID
+    var tabId = tabs[0].id;
 
-  paragraphs.forEach((paragraph) => {
-    textContent += ' ' + paragraph.innerText;
+    chrome.scripting.executeScript(
+      {
+        target: { tabId: tabId },
+        function: () => {
+          return document.querySelectorAll("p");
+        },
+      },
+      (results) => {
+        console.log("results", results);
+        var paragraphs = results;
+
+        let textContent = "";
+
+        paragraphs.forEach((paragraph) => {
+          textContent += " " + paragraph.innerText;
+        });
+
+        return textContent;
+      }
+    );
   });
-
-  return textContent;
 }
-
-// Parse the page content and log it
