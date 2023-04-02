10b906489359555c2bda513c68796653c6cfcb1d
Web Dev Cody <webdevcody@gmail.com>
2023-04-02 11:08:51 -0400

    adding remaining logic to display the response in the extension

--- extension/popup.js
+++ extension/popup.js
@@ -1,49 +1,54 @@
+let responseElement;
+
 document.addEventListener("DOMContentLoaded", function () {
   const btnAction = document.getElementById("btnAction");
+  responseElement = document.getElementById("response");
   btnAction.addEventListener("click", async function () {
-    const text = parsePageContent();
-    const response = await summarizeText(text);
-    console.log("Response:", response);
+    const text = await parsePageContent();
+    const { completion } = await summarizeText(text);
+    responseElement.innerText = completion;
   });
 });
 
 function parsePageContent() {
-  // Get the current tab
-  chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-    // Get the current tab's URL and ID
-    var tabId = tabs[0].id;
+  return new Promise((resolve) => {
+    // Get the current tab
+    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+      // Get the current tab's URL and ID
+      var tabId = tabs[0].id;
 
-    chrome.scripting.executeScript(
-      {
-        target: { tabId: tabId },
-        function: () => {
-          const paragraphs = document.querySelectorAll("p");
-          let textContent = "";
+      chrome.scripting.executeScript(
+        {
+          target: { tabId: tabId },
+          function: () => {
+            const paragraphs = document.querySelectorAll("p");
+            let textContent = "";
 
-          paragraphs.forEach((paragraph) => {
-            textContent += " " + paragraph.innerText;
-          });
+            paragraphs.forEach((paragraph) => {
+              textContent += " " + paragraph.innerText;
+            });
 
-          return textContent;
+            return textContent;
+          },
         },
-      },
-      (results) => {
-        if (chrome.runtime.lastError) {
-          console.error(JSON.stringify(chrome.runtime.lastError));
-          return;
+        (results) => {
+          if (chrome.runtime.lastError) {
+            console.error(JSON.stringify(chrome.runtime.lastError));
+            return;
+          }
+
+          const pageContent = results[0].result;
+          console.log("Page content:", pageContent);
+
+          resolve(pageContent);
         }
-
-        const pageContent = results[0].result;
-        console.log("Page content:", pageContent);
-
-        return pageContent;
-      }
-    );
+      );
+    });
   });
 }
 
 async function summarizeText(text) {
-  console.log("Summarizing text:", text)
+  console.log("Summarizing text:", text);
   // Send an HTTP request to the specified URL
   try {
     const response = await fetch("http://localhost:8080/text-completion", {
--- server/index.js
+++ server/index.js
@@ -7,35 +7,34 @@ const app = express();
 
 app.use(express.json());
 
-app.post('/text-completion', async (req, res) => {
+app.post("/text-completion", async (req, res) => {
   const prompt = req.body.prompt;
 
-  console.log("Prompt:", prompt)
+  console.log("Prompt:", prompt);
 
   try {
     const response = await axios.post(
-      'https://api.openai.com/v1/engines/davinci-codex/completions',
+      "https://api.openai.com/v1/completions",
       {
-        prompt: prompt,
-        max_tokens: 50,
-        n: 1,
-        stop: null,
-        temperature: 0.7,
+        model: "text-davinci-003",
+        prompt: `please summarize the following blog post article: ${prompt}`,
+        max_tokens: 1000,
       },
       {
         headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${process.env.OPENAPI_KEY}`,
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.OPENAI_KEY}`,
         },
       }
     );
 
     const completion = response.data.choices[0].text;
     res.json({ completion });
-
   } catch (error) {
     console.error(error.message);
-    res.status(500).json({ error: 'An error occurred while fetching completion.' });
+    res
+      .status(500)
+      .json({ error: "An error occurred while fetching completion." });
   }
 });
 
